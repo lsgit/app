67cd6608544dcf68a7da53898a03f122818f7677
lsgit <848949609@qq.com day06 lsgit>
2014-11-05 21:43:48 +0800

    init

---
+++ FindWC/FindWC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FindWC
+//
+//  Created by liushuai on 14/11/5.
+//  Copyright (c) 2014年 Liushuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FindWC/FindWC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FindWC
+//
+//  Created by liushuai on 14/11/5.
+//  Copyright (c) 2014年 Liushuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
